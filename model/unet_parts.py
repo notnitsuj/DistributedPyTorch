fb351cd02d8c1f57cdd64277bfaa11893f5b9482
notnitsuj <ngmphuong26@gmail.com>
2022-03-16 22:43:44 +0700

    Update model -> smaller

--- model/unet_parts.py
+++ model/unet_parts.py
@@ -19,66 +19,59 @@ class conv_block(nn.Module):
 class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = conv_block(3, 64)
-        self.conv2 = conv_block(64, 128)
-        self.conv3 = conv_block(128, 256)
-        self.conv4 = conv_block(256, 512)
+        self.conv1 = conv_block(3, 32)
+        self.conv2 = conv_block(32, 64)
+        self.conv3 = conv_block(64, 128)
+        self.conv4 = conv_block(128, 256)
         self.maxpool = nn.MaxPool2d(2, 2)
 
     def forward(self, x):
-        out = []
-
         conv1 = self.conv1(x)
-        out.append(conv1)
-        conv1 = self.maxpool(conv1)
+        pool1 = self.maxpool(conv1)
 
-        conv2 = self.conv2(conv1)
-        out.append(conv2)
-        conv2 = self.maxpool(conv2)
+        conv2 = self.conv2(pool1)
+        pool2 = self.maxpool(conv2)
 
-        conv3 = self.conv3(conv2)
-        out.append(conv3)
-        conv3 = self.maxpool(conv3)
+        conv3 = self.conv3(pool2)
+        pool3 = self.maxpool(conv3)
 
-        conv4 = self.conv4(conv3)
-        out.append(conv4)
-        conv4 = self.maxpool(conv4)
-        out.append(conv4)
-
-        return out
-
+        conv4 = self.conv4(pool3)
+        out = self.maxpool(conv4)
+        
+        return out, conv4, conv3, conv2, conv1
+      
 class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = conv_block(1024, 512)
-        self.conv2 = conv_block(512, 256)
-        self.conv3 = conv_block(256, 128)
-        self.conv4 = conv_block(128, 64)
+        self.conv1 = conv_block(512, 256)
+        self.conv2 = conv_block(256, 128)
+        self.conv3 = conv_block(128, 64)
+        self.conv4 = conv_block(64, 32)
 
-        self.deconv1 = nn.ConvTranspose2d(1024, 512, 2, 2)
-        self.deconv2 = nn.ConvTranspose2d(512, 256, 2, 2)
-        self.deconv3 = nn.ConvTranspose2d(256, 128, 2, 2)
-        self.deconv4 = nn.ConvTranspose2d(128, 64, 2, 2)
+        self.deconv1 = nn.ConvTranspose2d(512, 256, 2, 2)
+        self.deconv2 = nn.ConvTranspose2d(256, 128, 2, 2)
+        self.deconv3 = nn.ConvTranspose2d(128, 64, 2, 2)
+        self.deconv4 = nn.ConvTranspose2d(64, 32, 2, 2)
 
-    def forward(self, enc_output):
-        out = self.deconv1(enc_output[0])
-        enc_output[1] = CenterCrop((out.shape[2], out.shape[3]))(enc_output[1])
-        out = torch.cat((enc_output[1], out), dim = 1)
+    def forward(self, x, conv1, conv2, conv3, conv4):
+        out = self.deconv1(x)
+        conv1 = CenterCrop((out.shape[2], out.shape[3]))(conv1)
+        out = torch.cat((conv1, out), dim = 1)
         out = self.conv1(out)
 
         out = self.deconv2(out)
-        enc_output[2] = CenterCrop((out.shape[2], out.shape[3]))(enc_output[2])
-        out = torch.cat((enc_output[2], out), dim = 1)
+        conv2 = CenterCrop((out.shape[2], out.shape[3]))(conv2)
+        out = torch.cat((conv2, out), dim = 1)
         out = self.conv2(out)
 
         out = self.deconv3(out)
-        enc_output[3] = CenterCrop((out.shape[2], out.shape[3]))(enc_output[3])
-        out = torch.cat((enc_output[3], out), dim = 1)
+        conv3 = CenterCrop((out.shape[2], out.shape[3]))(conv3)
+        out = torch.cat((conv3, out), dim = 1)
         out = self.conv3(out)
 
         out = self.deconv4(out)
-        enc_output[4] = CenterCrop((out.shape[2], out.shape[3]))(enc_output[4])
-        out = torch.cat((enc_output[4], out), dim = 1)
+        conv4 = CenterCrop((out.shape[2], out.shape[3]))(conv4)
+        out = torch.cat((conv4, out), dim = 1)
         out = self.conv4(out)
 
         return out
