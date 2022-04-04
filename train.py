41c790ac409e3c43c9283539204b328838369487
notnitsuj <ngmphuong26@gmail.com>
2022-04-04 21:30:48 +0700

    Add DDP (draft)

--- train.py
+++ train.py
@@ -5,9 +5,11 @@ import sys
 import pandas as pd
 
 import torch
+import torch.multiprocessing as mp
+
 from model import UNet
 from utils.utils import Loss, set_seed
-from utils.train_utils import fit_1GPU, fit_DP
+from utils.train_utils import fit_1GPU, fit_DP, fit_DDP
 
 warnings.filterwarnings("ignore")
 
@@ -24,14 +26,17 @@ def get_args():
 
 def train(model, train_method: str, criterion, epochs=10, batch_size=4, 
             learning_rate=1e-4, val_percent=0.1, newsize=[960, 640]):      
-    if train_method == 'singleGPU':
-        fit = fit_1GPU
-    elif train_method == 'DP':
-        fit = fit_DP
-
     try:
-        model, train_losses, val_losses = fit(model, criterion=criterion, epochs=epochs, batch_size=batch_size, 
-                                                learning_rate=learning_rate, val_percent=val_percent, newsize=newsize)
+        if train_method == 'singleGPU':
+            model, train_losses, val_losses = fit_1GPU(model, criterion=criterion, epochs=epochs, batch_size=batch_size, 
+                                                        learning_rate=learning_rate, val_percent=val_percent, newsize=newsize)
+        elif train_method == 'DP':
+            model, train_losses, val_losses = fit_DP(model, criterion=criterion, epochs=epochs, batch_size=batch_size, 
+                                                        learning_rate=learning_rate, val_percent=val_percent, newsize=newsize)
+        elif train_method == 'DDP':
+            mp.spawn(fit_DDP, args=(2, 'nccl', model, criterion, epochs, batch_size, learning_rate, val_percent, newsize),
+                        nprocs=2, join=True)
+        
         torch.save(model.state_dict(), './checkpoints/'+args.train_method+'.pth')
         train_losses_df = pd.DataFrame(train_losses, columns=['Step', 'Loss'])
         train_losses_df.to_pickle('./loss/'+args.train_method+'/train_loss.pkl')
@@ -63,5 +68,4 @@ if __name__ == '__main__':
         n_gpus = torch.cuda.device_count()
         assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
 
-        if args.train_method == 'DP':
-            train(model, args.train_method, criterion)
+        train(model, args.train_method, criterion)
--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,12 +1,17 @@
 import numpy as np
 import torch
-from torch import optim
-from torch.nn import Module
-from torch.nn.parallel import DataParallel as DP, DistributedDataParallel as DDP
-from torch.utils.data import DataLoader, random_split
+import os
 import tqdm
 import logging
 from pathlib import Path
+
+from torch import optim
+from torch.nn import Module
+import torch.distributed as dist
+from torch.nn.parallel import DataParallel as DP, DistributedDataParallel as DDP
+from torch.utils.data.distributed import DistributedSampler
+
+from torch.utils.data import DataLoader, random_split
 from dataloading import CarvanaDataset, BasicDataset
 from evaluate import evaluate
 
@@ -36,7 +41,7 @@ def fit_1GPU(model: Module, criterion, epochs: int = 10, batch_size: int = 4,
     train_loader = DataLoader(train_set, shuffle=True, **loader_args)
     val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
 
-    # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss
+    # 4. Set up the optimizer and the learning rate scheduler
     optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-8)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, verbose=True)
     global_step = 0
@@ -70,13 +75,6 @@ def fit_1GPU(model: Module, criterion, epochs: int = 10, batch_size: int = 4,
                     mean_loss = np.mean(losses[-10:])
                     #print(f' At step {global_step}, the training loss is {mean_loss}.')
                     train_losses.append([global_step, mean_loss])
-
-                if global_step % 200 == 0:
-                    # Evaluate regularly to prevent overfitting
-                    val_loss = evaluate(model, dataloader=val_loader, criterion=criterion)
-                    val_losses.append([global_step, val_loss])
-                    #print(f' At step {global_step}, the validation loss is {val_loss}.')
-                    scheduler.step(val_loss)
 
         # Evaluate after each epoch
         val_loss = evaluate(model, dataloader=val_loader)
@@ -110,7 +108,7 @@ def fit_DP(model: Module, criterion, epochs: int = 10, batch_size: int = 4,
     train_loader = DataLoader(train_set, shuffle=True, **loader_args)
     val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
 
-    # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss
+    # 4. Set up the optimizer and the learning rate scheduler
     optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-8)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, verbose=True)
     global_step = 0
@@ -145,15 +143,80 @@ def fit_DP(model: Module, criterion, epochs: int = 10, batch_size: int = 4,
                     #print(f' At step {global_step}, the training loss is {mean_loss}.')
                     train_losses.append([global_step, mean_loss])
 
-                if global_step % 200 == 0:
-                    # Evaluate regularly to prevent overfitting
-                    val_loss = evaluate(model, dataloader=val_loader, criterion=criterion)
-                    val_losses.append([global_step, val_loss])
-                    #print(f' At step {global_step}, the validation loss is {val_loss}.')
-                    scheduler.step(val_loss)
-
         # Evaluate after each epoch
         val_loss = evaluate(model, dataloader=val_loader)
         val_losses.append([global_step, val_loss])
         #print(f' After {epoch} epoch, the validation loss is {val_loss}.')
         scheduler.step(val_loss)
+
+    return model, train_losses, val_losses
+
+
+def fit_DDP(rank, world_size, backend, model: Module, criterion, epochs: int = 10, batch_size: int = 4, 
+            learning_rate: float = 1e-4, val_percent: float = 0.1, newsize = [960, 640]):
+    
+    # Initialize the distributed environment
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29500'
+    dist.init_process_group(backend=backend, rank=rank, world_size=world_size)
+
+    # Create dataset
+    try:
+        dataset = CarvanaDataset(dir_img, dir_mask, newsize)
+        logging.info(f'Carvana Dataset')
+    except (AssertionError, RuntimeError):
+        dataset = BasicDataset(dir_img, dir_mask, newsize)
+        logging.info(f'Basic Dataset')
+
+    # 2. Split into train/validation partitions
+    n_val = int(len(dataset) * val_percent)
+    n_train = len(dataset) - n_val
+    train_set, val_set = random_split(dataset, [n_train, n_val], generator=torch.Generator())
+
+    # Data Loader
+    train_sampler = DistributedSampler(train_set, rank=rank, num_replicas=world_size)
+    train_loader = DataLoader(dataset, batch_size=batch_size/world_size, shuffle=False, sampler=train_sampler)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=4, drop_last=True, pin_memory=True)
+
+    # Model
+    model.cuda(rank)
+    model = DDP(model, device_ids=[rank])
+
+    # Set up the optimizer and the learning rate scheduler
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate*world_size, weight_decay=1e-8)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, verbose=True)
+    global_step = 0
+
+    # Start training
+    train_losses = []
+    val_losses = []
+    for epoch in range(epochs):
+        model.train()
+        losses = []
+
+        for batch in train_loader:
+            images = batch['image'].cuda().dtype(torch.float32)
+            true_masks = batch['mask'].cuda().dtype(torch.float32)
+
+            pred_masks = model(images)
+            loss = criterion(pred_masks, true_masks) 
+            
+            optimizer.zero_grad()
+            losses.append(loss.item())       
+
+            (batch_size * loss).backward()
+            optimizer.step()
+
+            global_step += 1
+
+        if global_step % 10 == 0 and rank == 0:
+            mean_loss = np.mean(losses[-10:])
+            #print(f' At step {global_step}, the training loss is {mean_loss}.')
+            train_losses.append([global_step, mean_loss])
+
+        if rank == 0:
+            # Evaluate after each epoch
+            val_loss = evaluate(model, dataloader=val_loader)
+            val_losses.append([global_step, val_loss])
+            #print(f' After {epoch} epoch, the validation loss is {val_loss}.')
+            scheduler.step(val_loss)
